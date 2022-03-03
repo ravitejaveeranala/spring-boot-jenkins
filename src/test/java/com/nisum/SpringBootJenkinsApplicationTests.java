ee84c2e73db09e7094fb9ebf8ae5ab730eccc6a1
ravitejasoft <ravitejav1202@gmail.com>
2022-03-03 15:13:03 +0530

    first commit

---
+++ src/test/java/com/nisum/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.nisum;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case Executed..");
+		assertEquals(true, true);
+	}
+
+}
